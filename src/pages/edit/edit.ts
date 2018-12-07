c3f673ba8504276bb59baef43e65fa225be2e08b
awstin17 <austinwiese.acw@gmail.com>
2018-12-07 00:18:43 -0800

    a little cleanup

--- src/pages/edit/edit.ts
+++ src/pages/edit/edit.ts
@@ -17,11 +17,11 @@ import { ImageProvider } from '../../providers/image/image';
 })
 export class EditPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public _image: ImageProvider, public viewCtrl : ViewController) {this.index = this.navParams.get("i");}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public _image: ImageProvider, public viewCtrl: ViewController) { this.index = this.navParams.get("i"); }
 
   index: any;
-  constraints: any = { audio: false, video: { facingMode: "user", width: 500, height: 500 } }; 
-  
+  constraints: any = { audio: false, video: { facingMode: "user", width: 500, height: 500 } };
+
   screenshotButton: any = document.querySelector('#screenshot-button');
   // img: any = document.querySelector('#imageimage');
   @ViewChild('screenshotimage') img;
@@ -30,25 +30,26 @@ export class EditPage {
 
   ionViewDidEnter() {
     navigator.mediaDevices.getUserMedia(this.constraints)
-.then(function(mediaStream) {
-  document.querySelector('video').srcObject = mediaStream;
-  document.querySelector('video').onloadedmetadata = function(e) {
-    document.querySelector('video').play();
-  };
-})
-.catch(function(err) { console.log(err.name + ": " + err.message); });
+      .then(function (mediaStream) {
+        document.querySelector('video').srcObject = mediaStream;
+        document.querySelector('video').onloadedmetadata = function (e) {
+          document.querySelector('video').play();
+        };
+      })
+      .catch(function (err) { console.log(err.name + ": " + err.message); });
   }
 
   hideVideoandScreenshot() {
-      this.video.nativeElement.style.display = "none";
-      // this.img.nativeElement.style.display = "none";
-      // this._image.show = 'home';
+    this.video.nativeElement.style.display = "none";
+    // this.img.nativeElement.style.display = "none";
+    // this._image.show = 'home';
   }
 
   saveToDatabase() {
 
-      this._image.postImage()
-      .subscribe((res) => { alert("successfully posted!");
+    this._image.postImage()
+      .subscribe((res) => {
+        alert("successfully posted!");
         this.dismissModal();
         // this._image.images.push(this._image.image);
         let data = res;
@@ -61,8 +62,8 @@ export class EditPage {
 
         (err) => alert("photo unsuccessfully uploaded to database")
       )
-    
-    
+
+
   }
 
   saveFromEditing() {
@@ -72,16 +73,17 @@ export class EditPage {
     this._image.editImage.subtitle = this._image.image.subtitle;
     this._image.editImage.description = this._image.image.description;
     this._image.saveImage(userId, imgId)
-      .subscribe((res) => {alert("successful edit"); this.dismissModal()
-      this._image.images[this.index].subtitle = this._image.editImage.subtitle;
-      this._image.images[this.index].description = this._image.editImage.description;
-      this._image.editImage.subtitle = '';
-      this._image.editImage.description = '';
-      this._image.image.fileName = '';
+      .subscribe((res) => {
+        alert("successful edit"); this.dismissModal()
+        this._image.images[this.index].subtitle = this._image.editImage.subtitle;
+        this._image.images[this.index].description = this._image.editImage.description;
+
+        //The part below clears image object in the image service mostly so the subtitle and description values don't persist if you try to edit other photos
+        this._image.image.fileName = '';
         this._image.image.subtitle = '';
         this._image.image.description = '';
-    },
-      (err) => alert("you suck at editing photos")
+      },
+        (err) => alert("you suck at editing photos")
       )
   }
 
@@ -89,15 +91,13 @@ export class EditPage {
     this.viewCtrl.dismiss();
   }
 
-  hello() {
-    console.log('hello');
+  takeScreenshot() {
     this.canvas.width = this.video.nativeElement.videoWidth;
-  this.canvas.height = this.video.nativeElement.videoHeight;
-  this.canvas.getContext('2d').drawImage(this.video.nativeElement, 0, 0);
-  let base64Image = this.canvas.toDataURL('data:image/jpeg;base64,');
-  this.img.nativeElement.src = base64Image;
-  console.log(base64Image);
-  this._image.image.fileName = base64Image;
+    this.canvas.height = this.video.nativeElement.videoHeight;
+    this.canvas.getContext('2d').drawImage(this.video.nativeElement, 0, 0);
+    let base64Image = this.canvas.toDataURL('data:image/jpeg;base64,');
+    this.img.nativeElement.src = base64Image;
+    this._image.image.fileName = base64Image;
   }
 
 }
