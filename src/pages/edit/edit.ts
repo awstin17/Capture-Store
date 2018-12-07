e47448628e9c8398c8d0a6327a7b2861e66f0886
awstin17 <austinwiese.acw@gmail.com>
2018-12-07 15:53:54 -0800

    got rid of constraints. made capture too slow

--- src/pages/edit/edit.ts
+++ src/pages/edit/edit.ts
@@ -23,9 +23,7 @@ export class EditPage {
   screenshotDisplay: any;
   videoDisplay: any = true;
   editFieldsDisplay: any;
-  constraints: any = { audio: false, video: { facingMode: "user", width: 1920,
-  height: 1080,
-  aspectRatio: 1.777777778 } };
+  constraints: any = { audio: false, video: { facingMode: "user"} };
 
   screenshotButton: any = document.querySelector('#screenshot-button');
   // img: any = document.querySelector('#imageimage');
