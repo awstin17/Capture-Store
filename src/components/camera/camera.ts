564b2f451f231b37967b46a371725874d77434e4
awstin17 <austinwiese.acw@gmail.com>
2019-03-06 13:05:31 -0800

    Cleaned up some code on camera component ts file

--- src/components/camera/camera.ts
+++ src/components/camera/camera.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Camera } from '@ionic-native/camera';
 import { ImageProvider } from '../../providers/image/image';
 import { ModalController } from 'ionic-angular';
 
@@ -17,34 +16,11 @@ import { EditPage } from '../../pages/edit/edit';
 })
 export class CameraComponent {
 
-  constructor(private camera: Camera, private _image: ImageProvider, public modalCtrl: ModalController) {}
+  constructor(private _image: ImageProvider, public modalCtrl: ModalController) {}
 
-  onClick() {
-    // let icon = document.getElementById("icon");
-    // icon.style.color = "#488aff"
-    // icon.style.fontSize = "65px";
-    // icon.style.left = "42.3%";
-
-    // let options = {
-    //   sourceType: this.camera.PictureSourceType.CAMERA,
-    //   destinationType: this.camera.DestinationType.DATA_URL
-    // };
-
-    // this.camera.getPicture(options).then((imageData) => {
-        // icon.style.color = "black"
-        // icon.style.fontSize = "60px";
-        // icon.style.left = "43%";
+  activateCamera() {
         this._image.show = "camera";
-        // this._image.image.fileName = 'data:image/jpeg;base64,' + imageData;
-        // this._image.photoTaken = true;
         this.presentEditModal();
-    // }, 
-    //   (err) => {
-    //     // icon.style.color = "black"
-    //     // icon.style.fontSize = "60px";
-    //     // icon.style.left = "43%";
-    //     alert("unsuccessful image capture");
-    // });
   }
 
   presentEditModal() {
